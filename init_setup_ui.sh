52781a2a3a5a701ed6cec452ed315b2c4e9d9f9c
esanvsh <sanvendra@gmail.com>
2024-08-27 21:31:56 +0530

    streamlit app sperate container config

---
+++ init_setup_ui.sh
@@ -0,0 +1,18 @@
+echo [$(date)]: "START"
+echo [$(date)]: "CREATING ENV with PYTHON3.11"
+# WITHOUT CONDA
+python3.11 -m venv venv
+# WITH CONDA
+#conda create --prefix ./env python=3.11 -y
+echo [$(date)]: "ACTIVATE ENV"
+# WITHOUT CONDA
+/bin/bash -c "source ./venv/bin/activate"
+# WITH CONDA
+#source activate ./env
+echo [$(date)]: "INSTALLING DEV requirements"
+# WITHOUT CONDA
+#python3.11 -m pip install -e .
+# WITH CONDA
+pip install -r requirements_ui_dev.txt
+#pip install -r requirements.txt
+echo [$(date)]: "END"
